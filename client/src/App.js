7e1854fc9a0efee576eb28536c570d1161289d47
MarcosGrizzuti <marcos.grizzuti@gmail.com>
2021-01-27 17:33:23 -0300

    Funcional final

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import { NavBar, Product, Catalogo, Status404, Home } from "./components/index";
+import { NavBar, Product, Catalogo, Home } from "./components/index";
 import { Provider } from "react-redux"
 import generarStore from "./store";
 
@@ -15,7 +15,6 @@ function App() {
             <Route exact path="/" component={Home} />
             <Route path="/catalogo" component={Catalogo} />
             <Route exact path="/producto/:id" component={Product} />
-            <Route component={Status404} /> {/* TODO: Hacerlo funcional */}
           </Switch>
         </Router>
       </Provider>
